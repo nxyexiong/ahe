2db894b0cf884921baaf07035cf6974cc811310e
Henry <nxyexiong@gmail.com>
2026-05-18 20:10:28 +0800

    update OslFwpKernelSetupPhase1 signature

--- src/AhePkg/Application/RootLoader/hooks.c
+++ src/AhePkg/Application/RootLoader/hooks.c
@@ -133,13 +133,13 @@ EFI_STATUS EFIAPI HookOslFwpKernelSetupPhase1(
     // in winload.efi:
     // OslMain(entry) -> OslpMain -> OslExecuteTransition -> OslFwpKernelSetupPhase1
     // OslpMain:
-    // - should be called at the end of OslMain with one parameter
+    // - OslpMain has 1 parameter
     // - the return value of OslMain should be the return value of OslpMain
     // OslExecuteTransition:
-    // - should be called at the near end of OslpMain without any parameter
-    // - should be called inside a check like if (v2 >= 0 && v8)
+    // - OslExecuteTransition has 0 parameters
+    // - the return value of OslpMain should be the return value of OslExecuteTransition
     // OslFwpKernelSetupPhase1:
-    // - should be called in the middle of OslExecuteTransition with one parameter
+    // - should be called in the middle of OslExecuteTransition with 1 parameter
     // - should be called after BlIpmiLogCheckPoint with the first parameter equals to 2
     // - it should be one of the few functions that calls BlFwGetSystemTable with
     //   the first parameter equals to FW_TABLE_RSDT(1)
@@ -158,10 +158,10 @@ EFI_STATUS EFIAPI HookOslFwpKernelSetupPhase1(
     // find OslFwpKernelSetupPhase1 in OslExecuteTransition
     // 48 8B CE: mov rcx, rsi ; LoaderBlock
     // E8 xxxx: call OslFwpKernelSetupPhase1
-    // 8B D8: mov ebx, eax
+    // 8B F8: mov edi, eax
     // 85 C0: test eax, eax
     Addr = FindPattern(Addr, 0x100,
-        "\x48\x8B\xCE\xE8\x00\x00\x00\x00\x8B\xD8\x85\xC0", "xxxx????xxxx");
+        "\x48\x8B\xCE\xE8\x00\x00\x00\x00\x8B\xF8\x85\xC0", "xxxx????xxxx");
     if (!Addr) return EFI_NOT_FOUND;
     CallOffset = *(INT32*)((UINT8*)Addr + 4);
     Addr = (VOID*)((UINT8*)Addr + 8 + CallOffset);
